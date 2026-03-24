7d1e47b6ec3c3761658444b1b457d3d27fb38b37
Sebastian <86731651+bocanadas@users.noreply.github.com>
2026-03-24 23:48:22 +0000

    Implement producer section

--- tech_lab_on_campus/market_watch/producer_and_consumer/producer/producer_interface.py
+++ tech_lab_on_campus/market_watch/producer_and_consumer/producer/producer_interface.py
@@ -12,28 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 class mqProducerInterface:
     def __init__(self, routing_key: str, exchange_name: str) -> None:
-        # Save parameters to class variables
-
-        # Call setupRMQConnection
         pass
 
     def setupRMQConnection(self) -> None:
-        # Set-up Connection to RabbitMQ service
-
-        # Establish Channel
-
-        # Create the exchange if not already present
-
         pass
 
     def publishOrder(self, message: str) -> None:
-        # Basic Publish to Exchange
-
-        # Close Channel
-
-        # Close Connection
-    
         pass
